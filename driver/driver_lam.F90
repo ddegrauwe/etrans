c516e8554d96f98d397becd135a9701712926f9e
Harold Petithomme <harold.petithomme@meteo.fr>
2023-11-16 18:53:31 +0000

    Improve options of program + syntax

--- driver/driver_lam.F90
+++ driver/driver_lam.F90
@@ -1,76 +1,31 @@
-PROGRAM DRIVER_LAM
+program driver_lam
+	use parkind1,only: jpim,jprb,jprd
+	use mpl_module
+	use yomgstats
 
-!
-! Spectral transform test
-!
-! This test performs spectral to real and real to spectral transforms repeated in
-! timed loop.
-!
-! 
-! Author : George Mozdzynski
-! Modifications: Daan Degrauwe (adapted global to LAM)
+	use cudafor
 
-USE PARKIND1, ONLY : JPIM, JPRB, JPRD
+	implicit none
 
-USE MPL_MODULE
-!USE GRIB_API
-USE YOMGSTATS
+	integer(kind=jpim) :: nerr,nulnam,nproc,nlin,nsmax,nq,nout,nspec2,ngptot,ngptotg,ifld,&
+		nproma,nblk,itag,nspec2g,myproc,jj,i,niters,nlon,nlat,nmsmax,ndgux,nlate,nprgpns,&
+		nprgpew,nprtrv,nprtrw,mysetv,jst,jnum,jprv,ifile,nfld,ngrib,jf,nfldsp,igrib,jp,&
+		jlat,kx,ky
+	integer :: jstep
+	integer(kind=jpim),allocatable :: nloen(:),ito(:),nprcids(:),ivset(:)
+	real(kind=jprb) :: ztinit,ztloop,ztstep,timef
+	real(kind=jprd) :: zaveave(0:jpmaxstat)
+	real(kind=jprb) :: zmaxerr_check=0,zdx,zdy
+	real(kind=jprb),parameter :: rpi=acos(-1._jprb)
+	real(kind=jprb),allocatable :: zspec(:,:),znorm(:),znorm1(:),zg(:,:),z(:,:,:)
+	logical :: lsplit,luserpnm,lkeeprpnm,luseflt,lltrace_stats,llstats_omp,llstats_comms,&
+		lnorms=.false.
+	character(len=256) :: cdatafile
 
-USE PARKIND_ECTRANS, ONLY : JPRBT
-
-use cudafor
-
-IMPLICIT NONE
-
-INTEGER(KIND=JPIM) :: RETURN_CODE
-
-INTEGER(KIND=JPIM),PARAMETER :: JPMLAT=8000
-
-INTEGER(KIND=JPIM) :: ISTACK, GETSTACKUSAGE
-REAL(KIND=JPRB)    :: ZERR,ZMAXERR
-
-INTEGER(KIND=JPIM) :: NERR,NULNAM,NPROC,NLIN,NSMAX,NQ
-INTEGER(KIND=JPIM) :: NOUT,NSPEC2,NGPTOT,NGPTOTG,IFLD
-INTEGER(KIND=JPIM) :: NPROMA, NBLK
-
-INTEGER(KIND=JPIM) :: ITAG,NSPEC2G,I
-INTEGER(KIND=JPIM) ,ALLOCATABLE :: NLOEN(:),ITO(:),NPRCIDS(:)
-INTEGER(KIND=JPIM) :: MYPROC,JJ
-INTEGER   :: JSTEP
-REAL(KIND=JPRB)    :: ZTINIT,ZTLOOP,ZTSTEP,TIMEF
-REAL(KIND=JPRB),ALLOCATABLE :: ZSPEC(:,:),ZNORM(:),ZNORM1(:)
-REAL(KIND=JPRB),ALLOCATABLE :: ZG(:,:), Z(:,:,:)
-REAL(KIND=JPRD) :: ZAVEAVE(0:JPMAXSTAT)
-
-LOGICAL :: LUSERPNM, LKEEPRPNM, LUSEFLT
-LOGICAL :: LLTRACE_STATS, LLSTATS_OMP, LLSTATS_COMMS
-LOGICAL :: LNORMS=.FALSE.
-INTEGER(KIND=JPIM) :: IITER, NITERS
-REAL(KIND=JPRB) :: ZMAXERR_CHECK=0.0_JPRB
-
-INTEGER(KIND=JPIM) :: NLON, NLAT, NMSMAX, NDGUX, NLATE
-REAL(KIND=JPRB) :: ZDX, ZDY
-INTEGER(KIND=JPIM) :: NPRGPNS, NPRGPEW, NPRTRV, NPRTRW, MYSETV
-INTEGER(KIND=JPIM) :: JST, JNUM, JPRV
-LOGICAL :: LSPLIT
-CHARACTER(LEN=256) :: CDATAFILE
-INTEGER(KIND=JPIM) :: IFILE, NFLD, NGRIB, JFLD, NFLDSP
-INTEGER(KIND=JPIM) :: IGRIB
-INTEGER(KIND=JPIM) :: JLON, JLAT, KX, KY
-REAL(KIND=JPRB), PARAMETER :: RPI=ACOS(-1._JPRB)
-INTEGER(KIND=JPIM), ALLOCATABLE :: IVSET(:)
-
-! Define namelists
-NAMELIST/NAMTRANS/ LBARRIER_STATS, LBARRIER_STATS2, LDETAILED_STATS, &
-                 & LUSERPNM, LKEEPRPNM, LUSEFLT, NQ, NLIN, &
-                 & LNORMS, NITERS, ZMAXERR_CHECK
-NAMELIST/NAMBIFFT/  NLATE, NFLD, NLON, NLAT, NPROMA
-NAMELIST/NAMCT0/ NPRGPNS, NPRGPEW, NPRTRW, NPRTRV
-
-
-REAL(KIND=JPRBT), ALLOCATABLE :: ZGTF(:,:)
-
-!     ------------------------------------------------------------------
+	namelist/namtrans/lbarrier_stats,lbarrier_stats2,ldetailed_stats,luserpnm,lkeeprpnm,&
+		luseflt,nq,nlin,lnorms,niters,zmaxerr_check
+	namelist/nambifft/nlate,nfld,nlon,nlat,nproma
+	namelist/namct0/nprgpns,nprgpew,nprtrw,nprtrv
 
 #include "setup_trans0.h"
 #include "esetup_trans.h"
@@ -79,241 +34,208 @@ REAL(KIND=JPRBT), ALLOCATABLE :: ZGTF(:,:)
 #include "edist_grid.h"
 #include "etrans_inq.h"
 #include "especnorm.h"
-!#include "abor1.intfb.h"
+#include "abor1.intfb.h"
 
-! Initializations
-NERR = 0
-NULNAM = 4
-NOUT = 6
+	nerr = 0
+	nulnam = 4
+	nout = 6
 
+	call mpl_init()
 
-! Message passing setup
-CALL MPL_INIT()
+	call gstats(0,0)
+	ztinit = timef()
 
+	nproc = mpl_nproc()
+	myproc = mpl_myrank()
+	if (myproc /= 1) open(nout,file="/dev/null")
 
-! Initialize statistics
-CALL GSTATS(0,0)
-ZTINIT=TIMEF()
+	write(nout,*) "NPROC:",nproc
 
-! Get number of procs and current proc
-NPROC=MPL_NPROC()
-MYPROC = MPL_MYRANK()
-IF(MYPROC == 1) THEN
-  WRITE(NERR,'(" NPROC=",I8)') NPROC
-ENDIF
+	allocate(nprcids(nproc))
+	do jj=1,nproc
+		nprcids(jj) = jj
+	end do
 
-! Array with proc id's
-ALLOCATE(NPRCIDS(NPROC))
-DO JJ=1,NPROC
-  NPRCIDS(JJ) = JJ
-ENDDO
+	lstats = .true.
+	ldetailed_stats = .false.
+	lstats_omp = .false.
+	lstats_comms = .false.
+	lstats_mpl = .false.
+	lbarrier_stats = .false.
+	lbarrier_stats2 = .false.
+	luserpnm = .false.
+	lkeeprpnm = .false.
+	luseflt = .true.
+	lnorms = .false.
+	niters = 10
 
-! Default statistics settings
-LSTATS=.TRUE.
-LDETAILED_STATS=.FALSE.
-LSTATS_OMP=.FALSE.
-LSTATS_COMMS=.FALSE.
-LSTATS_MPL=.FALSE.
-LBARRIER_STATS=.FALSE.
-LBARRIER_STATS2=.FALSE.
-LUSERPNM=.FALSE.
-LKEEPRPNM=.FALSE.
-LUSEFLT=.TRUE.
-LNORMS=.FALSE.
-NITERS=10
+	nlate = 0
+	nfld = 1
+	nlon = 48
+	nlat = 40
+	nproma = 16
 
-! Default BIFFT settings
-!CDATAFILE='data_bifft_200x180.grb'
-NLATE=0                                   ! matters for gridpoint distribution:
-                                          ! if NLATE=0, all procs have +/- same number of gridpoints.
-                                          ! if NLATE>0, procs treating the extension-zone have more gridpoints
-                                          !             (useful because physics calculations are not necessary in E-zone)
-NFLD=1
-NLON=48
-NLAT=40
-NPROMA=16
+	nprgpns = nproc
+	nprgpew = 1
+	nprtrw = nproc
+	nprtrv = 1
+	lsplit = .false.
 
-! Default distribution properties
-NPRGPNS=NPROC
-NPRGPEW=1
-NPRTRW=NPROC
-NPRTRV=1
-LSPLIT=.FALSE.
+	write(nout,*) "Read settings from namelist"
+	open(unit=nulnam,file="driver/driver.nam",form="formatted")
+	read(nulnam,namtrans)
+	rewind(nulnam)
+	read(nulnam,nambifft)
+	rewind(nulnam)
+	read(nulnam,namct0)
+	close(nulnam)
 
-! Read settings from namelist
-REWIND(NULNAM)
-READ(NULNAM,NAMTRANS)
-REWIND(NULNAM)
-READ(NULNAM,NAMBIFFT)
-REWIND(NULNAM)
-READ(NULNAM,NAMCT0)
+	write(nout,*) "Set up distribution: A/B sets"
+   if (nproc == 1) then
+		nprgpns = 1
+		nprgpew = 1
+	else
+		call setab(nproc,nprgpns,nprgpew)
+	   if (nprgpns*nprgpew /= nproc) call abor1("Error: no partition found for A/B sets")
+	end if
 
-WRITE (*,*) 'JPRB = ',JPRB
-WRITE (*,*) 'NLON = ',NLON,'; NLAT = ',NLAT
+	write(nout,*) "Distribution values (NS EW W V):",nprgpns,nprgpew,nprtrw,nprtrv
 
+	ndgux = nlat-nlate
 
-! Setup statistics
-IF(MYPROC == 1) THEN
-  WRITE(NOUT,'(" LDETAILED_STATS=",L1," LBARRIER_STATS=",L1," LBARRIER_STATS2=",L1)')&
-   & LDETAILED_STATS,LBARRIER_STATS,LBARRIER_STATS2
-ENDIF
-IF(LDETAILED_STATS)THEN
-  LSTATS_OMP=.TRUE.
-  LSTATS_COMMS=.TRUE.
-  LSTATS_MPL=.TRUE.
-ENDIF
-CALL GSTATS_SETUP(NPROC,MYPROC,NPRCIDS,&
- & LSTATS,LSTATSCPU,LSYNCSTATS,LDETAILED_STATS,LBARRIER_STATS,LBARRIER_STATS2,&
- & LLSTATS_OMP,LLSTATS_COMMS,LSTATS_MEM,NSTATS_MEM,LSTATS_ALLOC,&
- & LLTRACE_STATS,NTRACE_STATS,NPRNT_STATS,LXML_STATS,LSTATS_MPL)
-CALL GSTATS_PSUT
-!CALL GSTATS_LABEL_IFS
+	write(nout,*) "NLAT/NLON/NDGUX:",nlat,nlon,ndgux
 
-! Spectral truncation: linear -- maybe make this a namelist setting?
-NMSMAX=(NLON-1)/2
-NSMAX=(NLAT-1)/2
+	write(nout,*) "Setup statistics"
+	write(nout,'(" LDETAILED_STATS=",L1," LBARRIER_STATS=",L1," LBARRIER_STATS2=",&
+		L1)') ldetailed_stats,lbarrier_stats,lbarrier_stats2
+	if (ldetailed_stats) then
+		lstats_omp = .true.
+		lstats_comms = .true.
+		lstats_mpl = .true.
+	end if
 
-! Number of gridpoints w/o extension zone
-NDGUX=NLAT-NLATE
+	call gstats_setup(nproc,myproc,nprcids,lstats,lstatscpu,lsyncstats,ldetailed_stats,&
+		lbarrier_stats,lbarrier_stats2,llstats_omp,llstats_comms,lstats_mem,nstats_mem,&
+		lstats_alloc,lltrace_stats,ntrace_stats,nprnt_stats,lxml_stats,lstats_mpl)
+	call gstats_psut
 
-! Setup distribution
-ALLOCATE(NLOEN(NLAT))
-NLOEN(:) = NLON
-!write (*,*) __FILE__, __LINE__; call flush(6)
-CALL SETUP_TRANS0(KOUT=NOUT,KERR=NERR,KPRINTLEV=0,KMAX_RESOL=1,&
- & KPRGPNS=NPRGPNS,KPRGPEW=NPRGPEW,KPRTRW=NPRTRW)
+	nmsmax = (nlon-1)/2
+	nsmax = (nlat-1)/2
 
-!write (0,*) __FILE__, __LINE__,'; cudaDeviceSynchronize returns ',cudaDeviceSynchronize()
+	write(nout,*) "Setup distribution - part 0"
+	allocate(nloen(nlat))
+	nloen(:) = nlon
 
-! 1km resolution -- doesn't matter for FFT's
-ZDX=1.e3
-ZDY=1.e3
-!write (*,*) __FILE__, __LINE__; call flush(6)
-CALL ESETUP_TRANS(KMSMAX=NMSMAX,KSMAX=NSMAX,KDGL=NLAT,KDGUX=NDGUX, &
- & KLOEN=NLOEN,LDSPLIT=LSPLIT,PEXWN=ZDX,PEYWN=ZDY,LDUSEFFTW=.FALSE.)
+	call setup_trans0(kout=nout,kerr=nerr,kprintlev=0,kmax_resol=1,kprgpns=nprgpns,&
+		kprgpew=nprgpew,kprtrw=nprtrw)
 
-! Find distribution properties: total number of waves, number of waves on this proc, total number of gridpoints, gridpoints on this proc
-!write (*,*) __FILE__, __LINE__; call flush(6)
-CALL ETRANS_INQ(KSPEC2=NSPEC2,KSPEC2G=NSPEC2G,KGPTOT=NGPTOT,KGPTOTG=NGPTOTG,KMYSETV=MYSETV)
+	write(nout,*) "Setup distribution - 1km resolution"
+	zdx = 1.e3
+	zdy = 1.e3
+	call esetup_trans(kmsmax=nmsmax,ksmax=nsmax,kdgl=nlat,kdgux=ndgux,kloen=nloen,&
+		ldsplit=lsplit,pexwn=zdx,peywn=zdy,ldusefftw=.false.)
 
+	call etrans_inq(kspec2=nspec2,kspec2g=nspec2g,kgptot=ngptot,kgptotg=ngptotg,&
+		kmysetv=mysetv)
 
-! number of blocks
-NBLK=(NGPTOT-1)/NPROMA+1
+	nblk = (ngptot-1)/nproma+1
 
-! Setup vertical distribution: IVSET contains on which proc each field is; NFLDSP is the number of fields on MYPROC
-ALLOCATE(IVSET(NFLD))
-IF (NPRTRV==1) THEN
-  IVSET(:)=1
-  NFLDSP=NFLD
-ELSE
-  NFLDSP=0
-  JST=1
-  DO JPRV=1,NPRTRV
-    IF ( JPRV <= NFLD-NPRTRV*(NFLD/NPRTRV)) THEN
-      JNUM=NFLD/NPRTRV+1
-    ELSE
-      JNUM=NFLD/NPRTRV
-    ENDIF
-    IVSET(JST:JST+JNUM-1)=JPRV
-    IF (JPRV==MYSETV) NFLDSP=JNUM
-    JST=JST+JNUM
-  ENDDO
+	write(nout,*) "Runtime parameters:"
+	write(nout,'("ngptot, ngptotg ",2(x,i0))') ngptot,ngptotg
+	write(nout,'("nproma, nblk ",2(x,i0))') nproma,nblk
+	write(nout,'("nspec2, nspec2g ",2(x,i0))') nspec2,nspec2g
 
-      
-  !DO IFLD=1,NFLD
-  !  IVSET(IFLD)=MODULO(IFLD-1,NPRTRV)+1   ! not optimal: clustered (1,1,...,2,2,...) maybe better than scattered (1,2,1,2,...)
-  !  IF (IVSET(IFLD)==MYSETV) NFLDSP=NFLDSP+1
-  !ENDDO
+	allocate(ivset(nfld))
+	if (nprtrv == 1) then
+		ivset(:) = 1
+		nfldsp = nfld
+	else
+		nfldsp = 0
+		jst = 1
+		do jprv=1,nprtrv
+			if (jprv <= nfld-nprtrv*(nfld/nprtrv)) then
+				jnum = nfld/nprtrv+1
+			else
+				jnum = nfld/nprtrv
+			end if
+			ivset(jst:jst+jnum-1) = jprv
+			if (jprv == mysetv) nfldsp = jnum
+			jst = jst+jnum
+		end do
+	end if
 
-ENDIF
-      
-! Allocations for data
-ALLOCATE(ZSPEC(NFLDSP,NSPEC2))   ! Spectral coefficients (all fields)
-ALLOCATE(ZG(NGPTOTG,NFLD))      ! global gridpoint field
-ALLOCATE(Z(NPROMA,NFLD,NBLK))     ! local gridpoint field array (all fields)
+	write(nout,*) "Initialize spectral/gridpoint data, 3D fields:",nfld
+	allocate(zspec(nfldsp,nspec2))
+	allocate(zg(ngptotg,nfld),z(nproma,nfld,nblk))
 
-! Array necessary for distributing data from proc 1 to others
-ALLOCATE(ITO(NFLD))
+	allocate(ito(nfld))
 
-! first proc fills global array
-IF (MYPROC==1) THEN
-  KX=0
-  KY=1
-  
-  ZG=0.
-  DO JLAT=1,NLAT
-    DO JLON=1,NLON
-	  ZG((JLAT-1)*NLON+JLON,1) = COS( KX * 2*RPI*(REAL(JLON-1)/NLON) ) * COS ( KY * 2*RPI*(REAL(JLAT-1)/NLAT) )
-	ENDDO
-  ENDDO
-ENDIF
+	if (myproc == 1) then
+		kx = 0
+		ky = 1
 
-! Synchronize processors
-IF(NPROC > 1) THEN
-	CALL MPL_BARRIER()
-ENDIF
+		zg = 0
+		do jlat=1,nlat
+			do jp=1,nlon
+				zg((jlat-1)*nlon+jp,1) = cos(2*rpi*kx*(jp-1)/nlon)*cos(2*rpi*ky*(jlat-1)/nlat)
+			end do
+		end do
+	end if
 
-! Distribute gridpoint field
-ITO(:)=1
-!write (*,*) __FILE__, __LINE__; call flush(6)
+	if (nproc > 1) call mpl_barrier()
 
+	ito(:) = 1
 
-CALL EDIST_GRID(KPROMA=NPROMA,PGPG=ZG,KFDISTG=NFLD,KFROM=ITO,PGP=Z)
-!write (*,*) __FILE__, __LINE__; call flush(6)
+	call edist_grid(kproma=nproma,pgpg=zg,kfdistg=nfld,kfrom=ito,pgp=z)
 
+	write(nout,*) "Z(1:6):",z(1:6,1,1)
 
-write (*,*) 'before transform:'
-write (*,*) ' Z = ',Z(1:6,1,1),'...'
+	deallocate(zg)
 
-! Deallocate arrays
-DEALLOCATE(ZG)
+	allocate(znorm(nfld))
 
+	do i=1,niters
+		write(nout,"('. iteration',x,i0,'/',i0)") i,niters
+		call edir_trans(kproma=nproma,pspscalar=zspec,kvsetsc=ivset,pgp=z)
 
-! Allocate norm arrays
-ALLOCATE(ZNORM(NFLD))
+		if (lnorms) then
+			call especnorm(pspec=zspec,kvset=ivset,pnorm=znorm)
 
-DO IITER=1,NITERS
-! First spectral transform: we want to start the timeloop in spectral space: that's where the norms are calculated
-!write (*,*) __FILE__, __LINE__; call flush(6)
-CALL EDIR_TRANS(KPROMA=NPROMA,PSPSCALAR=ZSPEC,KVSETSC=IVSET,PGP=Z)
-!write (*,*) __FILE__, __LINE__; call flush(6)
+			do ifld=1,nfld
+				write(nout,'("ZNORM(",I4,"):",F20.15)') ifld,znorm(ifld)
+			end do
+		end if
 
-!write (*,*) 'ZSPEC = ',ZSPEC(1,1:6),'...'
+		write(nout,"('. inverse transforms')")
+		call einv_trans(kproma=nproma,pspscalar=zspec,kvsetsc=ivset,pgp=z)
+	end do
 
-! Calculate  and write initial norms
-IF (LNORMS) THEN
-	CALL ESPECNORM(PSPEC=ZSPEC,KVSET=IVSET,PNORM=ZNORM)
-	IF(MYPROC == 1) THEN
-		WRITE (NOUT,*)
-		DO IFLD=1,NFLD
-		  WRITE(NOUT,'(" ZNORM(",I4,")=",F20.15)') IFLD,ZNORM(IFLD)
-		ENDDO
-		WRITE (NOUT,*)
-	ENDIF
-ENDIF
+	write(nout,*) "ZSPEC:",zspec(1,1:5)
+	write(nout,*) "Z:",minval(z),sum(z)/size(z),maxval(z)
+	write(nout,*) "Z(1:6):",z(1:6,1,1)
 
-! inverse transform
-CALL EINV_TRANS(KPROMA=NPROMA,PSPSCALAR=ZSPEC,KVSETSC=IVSET,PGP=Z)
+	call mpl_barrier()
 
-!write (*,*) 'Z = ',Z(1:5,1,1),'...'
+	call gstats(0,1)
+	call gstats_print(nout,zaveave,jpmaxstat)
 
-ENDDO
+	call mpl_end()
+contains
+	subroutine setab(n,na,nb)
+		integer(kind=jpim),intent(in) :: n
+		integer(kind=jpim),intent(out) :: na,nb
 
-write (*,*) 'spectral:'
-write (*,*) ' ZSPEC = '
-write (*,'(5X,4F12.4)') ZSPEC(1,:)
+		integer(kind=jpim) :: ja,ib,isqr
 
-write (*,*) 'after transform:'
-write (*,*) ' Z = ',Z(1:6,1,1),'...'
+		isqr = sqrt(real(n))
 
+		do ja=isqr,n
+			ib = n/ja
+			if (ja*ib /= n) cycle
 
-! Close down message passing
-CALL MPL_BARRIER()
-
-
-CALL GSTATS(0,1)
-CALL GSTATS_PRINT(NOUT,ZAVEAVE,JPMAXSTAT)
-
-CALL MPL_END()
-
-END PROGRAM DRIVER_LAM
+			na = max(ja,ib)
+			nb = min(ja,ib)
+			exit
+		end do
+	end subroutine
+end program
